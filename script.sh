6343885a9926312138bcab8320f77f9cbc389d17
ilya <ilya@moskovko.com>
2015-09-22 14:35:34 -0700

    ** constants should be spelled with capital letters **

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-bin_name=$(basename $0)
+BIN_NAME=$(basename $0)
 
 show_usage() {
-    printf "Usage: ${bin_name} <"
-    cat ${bin_name} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # first-level-arg/ { print $2; }'
+    printf "Usage: ${BIN_NAME} <"
+    cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # first-level-arg/ { print $2; }'
     printf ">\n"
 }
 
