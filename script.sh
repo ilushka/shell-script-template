7a94078e7c20e276ae5faa8ce01083c55aef9589
ilya moskovko <ilya@empowermicro.com>
2016-02-05 13:22:18 -0800

    ** hid last separator ** added multi-level args **

--- script.sh
+++ script.sh
@@ -4,26 +4,47 @@ set -e
 
 BIN_NAME=$(basename $0)
 
-show_usage() {
-    printf "Usage: ${BIN_NAME} <"
-    cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # first-level-arg/ { print $2; }'
-    printf ">\n"
+function show_usage() {
+    case $1 in
+        "second")
+            printf "Usage: ${BIN_NAME} second <"
+            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # second-level-arg/ { print $2; }'
+            printf "\b>\n"  # \b (backspace) is to hide last separator
+            ;;
+        *)
+            printf "Usage: ${BIN_NAME} <"
+            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # first-level-arg/ { print $2; }'
+            printf "\b>\n"
+            ;;
+    esac
 }
 
-show_readme() {
+function show_readme() {
     printf "
 ## Quick Start
 "
 }
 
+function second_commands() {
+    case $1 in
+        "level") # second-level-arg;
+            printf "second level command\n"
+            ;;
+    esac
+}
+
 case $1 in
+    "second") # first-level-arg
+        second_commands "${@:2}"
+        ;;
     "help") # first-level-arg;
-        show_usage
+        show_usage "${@:2}"
         ;;
     "readme") # first-level-arg;
         show_readme
         ;;
     *)
-        show_usage
+        show_usage "${@:2}"
         ;;
 esac
+
