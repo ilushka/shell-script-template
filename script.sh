3150878efb53c4d47738960f6228793333d32ac1
ilya moskovko <ilya@moskovko.com>
2019-06-20 23:29:34 -0700

    ** updated the way usage is extracted for single and double commands **

--- script.sh
+++ script.sh
@@ -5,46 +5,61 @@ set -e
 BIN_NAME=$(basename $0)
 
 function show_usage() {
+    if [[ -z "$1" ]]; then
+        printf "Usage: ${BIN_NAME} <"
+        cat ${BIN_NAME} | sed -n 's/[[:space:]]*\"\([a-z0-9-]*\)\") # first-level-arg;/\1/p' | tr '\n' '|'
+        printf "\b>\n"  # \b (backspace) is to hide last separator
+    else
+        dbl_usage=$(cat script.sh  | sed -n 's/[[:space:]]*\"\([a-z0-9-]*\)\") # second-level-arg; '"${1}"'/\1/p' | tr '\n' '|')
+        if [[ ! -z "$dbl_usage" ]]; then
+            printf "Usage: ${BIN_NAME} ${1} <${dbl_usage}\b>\n"  # \b (backspace) is to hide last separator
+        else
+            printf "Usage: ${BIN_NAME} ${1}\n"
+        fi
+    fi
+}
+
+function single_command() {
+    printf "Single command\n"
+}
+
+function double_command1() {
     case $1 in
-        "second")
-            printf "Usage: ${BIN_NAME} second <"
-            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z\-]+\"\) # second-level-arg/ { print $2; }'
-            printf "\b>\n"  # \b (backspace) is to hide last separator
+        "foo") # second-level-arg; double-cmd1
+            ;;
+        "bar") # second-level-arg; double-cmd1
             ;;
         *)
-            printf "Usage: ${BIN_NAME} <"
-            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z\-]+\"\) # first-level-arg/ { print $2; }'
-            printf "\b>\n"
+            show_usage "double-cmd1"
             ;;
     esac
 }
 
-function show_readme() {
-    printf "
-## Quick Start
-"
-}
-
-function second_commands() {
+function double_command2() {
     case $1 in
-        "level") # second-level-arg;
-            printf "second level command\n"
+        "baz") # second-level-arg; double-cmd2
+            ;;
+        "qux") # second-level-arg; double-cmd2
+            ;;
+        *)
+            show_usage "double-cmd2"
             ;;
     esac
 }
-
 case $1 in
-    "second") # first-level-arg
-        second_commands "${@:2}"
+    "single") # first-level-arg;
+        single_command "${@:2}"
+        ;;
+    "double-cmd1") # first-level-arg;
+        double_command "${@:2}"
+        ;;
+    "double-cmd2") # first-level-arg;
+        double_command2 "${@:2}"
         ;;
     "help") # first-level-arg;
         show_usage "${@:2}"
-        ;;
-    "readme") # first-level-arg;
-        show_readme
         ;;
     *)
         show_usage "${@:2}"
         ;;
 esac
-
